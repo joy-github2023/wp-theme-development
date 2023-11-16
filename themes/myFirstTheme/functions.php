5db52815e07bb30e517200e19fa779e153cf9a39
joy-github2023 <joy@lexaeon.com>
2023-11-16 12:08:38 -0500

    Moved to Local!

--- themes/myFirstTheme/functions.php
+++ themes/myFirstTheme/functions.php
@@ -1,4 +1,33 @@
 <?php 
+
+function pageBanner($args = NULL) {
+    
+    if (!isset($args['title'])) {
+        $args['title'] = get_the_title();
+    }
+    if (!isset($args['subtitle'])) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+    if (!isset($args['photo'])) {
+        if(get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
+            $args['photo'] = get_field('page_banner_background_image');
+        } else {
+            $args['photo'] = get_theme_file_uri('images/ocean.jpg');
+        }
+    }
+
+    ?>
+    <div class="page-banner">
+		<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>)"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+			<div class="page-banner__intro">
+				<p><?php echo $args['subtitle'] ?></p>
+			</div>
+		</div>
+	</div>
+<?php }
+
 function university_files() {
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
@@ -12,7 +41,8 @@ function university_features() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260);
-    add_image_size('professorportrait', 480, 650);
+    add_image_size('professorPortrait', 480, 650);
+    add_image_size('pageBanner', 1500, 650);
     register_nav_menu('headerMenuLocation', 'Header Menu');
     register_nav_menu('footerMenuLocationOne', 'Footer Menu One');
     register_nav_menu('footerMenuLocationTwo', 'Footer Menu Two');
