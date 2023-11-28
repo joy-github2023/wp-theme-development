3db9b268dd0373256132a07277c922ed15d12296
joy-github2023 <joy@lexaeon.com>
2023-11-28 04:37:16 -0500

    Section 14 completed!

---
+++ themes/myFirstTheme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '67babef1a0a69e1bb220');
--- themes/myFirstTheme/functions.php
+++ themes/myFirstTheme/functions.php
@@ -34,6 +34,9 @@ function university_files() {
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_script('main_university_js', get_theme_file_uri('/build/index.js'), array('jquery'), 1.0, true);
+    wp_localize_script('main_university_js', 'universityData', array(
+        'root_url' => get_site_url()
+    ));
 }
 add_action('wp_enqueue_scripts', 'university_files');
 
